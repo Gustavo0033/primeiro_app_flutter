2d0a6bfd9050f9aa083c65da780e6c62f4c51272
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2023-01-24 15:04:24 -0300

    Primeiro app flutter

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Container(
+        color: Colors.blue,
+        alignment: Alignment.center,
+        child: Text('Olá Mundo'),
+      ),
+    ),
+  );
+}
+
+class myApp extends StatelessWidget{
+  const myApp({super.key});
+
+  Widget build(BuildContext context){
+
+  }
+}
