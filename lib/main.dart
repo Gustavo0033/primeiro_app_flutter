8fca76ba025bd5a33d22ad98df628bbd2be5baee
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2023-01-26 12:21:43 -0300

    Text "pode entrar"

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,43 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Container(
-        color: Colors.blue,
-        alignment: Alignment.center,
-        child: Text('Olá Mundo'),
-      ),
-    ),
-  );
+  runApp(const myApp());
 }
 
-class myApp extends StatelessWidget{
+class myApp extends StatelessWidget {
   const myApp({super.key});
 
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
 
+// Widget responsavel pela pagina do app
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+         Text(
+          "Pode entrar",
+          style: TextStyle(fontSize: 20,
+        fontWeight: FontWeight.w100,
+        color: Colors.white)),
+         Text("0",
+
+           style: TextStyle(fontSize: 30,
+             color: Colors.white,
+           ),
+         )
+      ],
+    ),
+    );
   }
 }
